1a598f46b0f4787dc48e1083508146fd28d15bed
penowa <penowa98@gmail.com>
2023-12-25 21:58:52 +0200

    first automated test

--- src/test/java/FirstTest/SuccessfulLogin.java
+++ src/test/java/FirstTest/SuccessfulLogin.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -29,12 +30,25 @@ public class SuccessfulLogin {
         driver.get("https://www.saucedemo.com");
 
         //create variables for different fields and buttons
-        //variable for field user name
+        //variable for field username
         WebElement userNameInput = driver.findElement(By.id("user-name"));
         //actions we do with the found element
         userNameInput.click();
         userNameInput.clear(); //it is good to always clear the field because there can be some old text
        // sendKeys - with this method we write in the field
-        userNameInput.sendKeys("standart_user");
+        userNameInput.sendKeys("standard_user");
+
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys("secret_sauce");
+
+        WebElement loginBtn = driver.findElement(By.cssSelector("[name=login-button]"));
+        loginBtn.click();
+
+        WebElement productTittle = driver.findElement(By.xpath("//span[contains(text(), 'Products')]"));
+        //We use assertions to validate if test passed or failed
+        //Assertions are given by TestNG
+        Assert.assertTrue(productTittle.isDisplayed());
     }
 }
