e6cf13dc92200829e22bb48c266b2e76a1ec5be9
penowa <penowa98@gmail.com>
2023-12-28 20:12:17 +0200

    first automated test

--- src/test/java/FirstTest/Login.java
+++ src/test/java/FirstTest/Login.java
@@ -1,5 +1,35 @@
 package FirstTest;
+/*
+get(String url) - Loads a new web page in the current browser window.
+findElement(By by) - Finds the first WebElement using the given method.
+findElements(By by) - Finds all elements within the current page.
+getCurrentUrl() - Gets a string representing the current URL in the browser
+getPageSource() - Gets the source of the last loaded page.
+getTitle() - The title of the current page.
+getWindowHandles() - Returns a set of window handles which can be used to iterate over all open windows of this WebDriver instance by passing them to switchTo()
+switchTo() - Sends future commands to a different frame or window.
+close() - Closes the current window, quitting the browser if it's the last window currently open.
+quit () - Quits this driver, closing every associated window.
+clear() - If this element is a text entry element, this will clear the value.
+click() - Click this element.
+sendKeys(String text) - Use this method to simulate typing into an element, which set its value.
+getAttribute(String) - Get the value of the given attribute of the element.
+getTagName() - Get the tag name of this element.
+getText() - Get the visible text of this element.
+isDisplayed() - Whether this element is displayed or not.
+isEnabled() - Whether this element is enabled or not.
+isSelected() - Whether this element is selected or not. Usually for checkboxes.
+submit() - If this current element is a form, or an element within a form, then this will be submitted to the remote server.
+clickAndHold() - Clicks (without releasing) at the current mouse location.
+click() - Clicks at the current mouse location.
+sendKeys(String text) - Send keys to the active element.
+doubleClick() - Performs a double-click at the current mouse location.
+dragAndDrop() - A convenience method that performs click-and-hold at the location of the source element, moves to the location of the target element, then releases the mouse.
+moveToElement(WebElement element) - Moves the mouse to the middle of the element.
+pause(long pause) - Performs a pause.
+build() - Generates a composite action containing all actions so far, ready to be performed
 
+ */
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
--- src/test/java/FirstTest/ProductTest.java
+++ src/test/java/FirstTest/ProductTest.java
@@ -2,15 +2,17 @@ package FirstTest;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ProductTest extends TestUtil{
-    //
-    private final static String PRODUCT_ID = "add-to-cart-sauce-labs-";
+    //first part of the product id, second is given in data provider
+    private final static String BASE_PRODUCT_ID = "add-to-cart-sauce-labs-";
 
-    @Test
-    public void AddProductToCart(){
+    //what we read from data provider is accepted as parameter in the method
+    @Test (dataProvider = "items list")
+    public void AddProductToCart(String itemName){
        //First - login
         WebElement userNameInput = driver.findElement(By.id("user-name"));
         userNameInput.click();
@@ -24,12 +26,21 @@ public class ProductTest extends TestUtil{
 
         WebElement loginBtn = driver.findElement(By.cssSelector("[name=login-button]"));
         loginBtn.click();
+
+        WebElement itemToBeAdded = driver.findElement(By.id(BASE_PRODUCT_ID + itemName));
+        itemToBeAdded.click();
+
+        WebElement shoppingCartBadge = driver.findElement(By.xpath("//span[@class='shopping_cart_badge']"));
+
+        Assert.assertEquals(shoppingCartBadge.getText(), "1");
     }
 
+    //the test will be executed for all data(items) in Data Provider
     @DataProvider(name = "items list")
     public Object[][] getItems(){
         return new Object[][]{
-                {"bike-light"}
-        }
+                {"bike-light"},
+                {"bolt-t-shirt"}
+        };
     }
 }
